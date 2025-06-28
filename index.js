994804db109b9e36cb07d0b242c3c202c1730170
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-06-28 22:56:42 +0300

    chore: update readme

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import program from "caporal";
 import { create } from "./create-symlinks.js";
 import { remove } from "./remove-symlinks.js";
 
-program.name("spica-symlink").version("1.0.5").description("Symlink tool to replace folder IDs with readable titles in Spica.");
+program.name("spica-symlink").version("1.1.1").description("Symlink tool to replace folder IDs with readable titles in Spica.");
 
 program
   .command("create", "Generate symlinks for folders to display readable titles")
